473c1abed4ac6fadce325ead3426097e76c30499
Didier Simen <dcsimen@gmail.com>
2022-11-28 20:07:54 +0100

    Update routes.py
    added logic app call

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from .forms import AddUser, Login
 from .models import Users 
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
+import requests
 
 main = Blueprint('main', __name__)
 
@@ -20,7 +21,7 @@ def add_user():
             check = Users.query.filter_by(email_address = form.email_address.data).first()
             if check : 
                 flash('Email already exists', category='danger')
-                request.post("")
+                requests.post(f"https://prod-07.centralus.logic.azure.com/workflows/2c29915665b34973a0223eca1ea86a66/triggers/manual/paths/invoke/{form.first_name.data}/{form.last_name.data}/{form.email.data}?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ppS68I1_ePRmJqxfP4aY5wpEHKWlxCnowUPJRG1cMGY")
                 return redirect(url_for('main.add_user'))
             else:
                 Users(last_name = form.last_name.data, first_name = form.first_name.data, email_address = form.email_address.data, password_hash = generate_password_hash(form.password_hash.data, method='sha256')).save_to_db()
